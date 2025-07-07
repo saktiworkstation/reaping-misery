f453de186d991b38bd85c530857301f85023c39d
Rplx197 <brafli99197@gmail.com>
2025-07-07 11:45:36 +0700

    test icon

--- components/Features.vue
+++ components/Features.vue
@@ -25,7 +25,7 @@
 // Feature icons as components
 const LeafIcon = {
   template: `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg xmlns="https://img.icons8.com/?size=100&id=undefined&format=png&color=000000" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
     </svg>
   `
