b49a33faf61b3c15e5f86f963c0166ef417d8269
Rplx197 <brafli99197@gmail.com>
2025-07-07 12:04:07 +0700

    comment wishlist button

--- components/Hero.vue
+++ components/Hero.vue
@@ -25,14 +25,14 @@
             </svg>
             Download Sekarang
           </a>
-          <button class="btn-secondary">
+          <!-- <button class="btn-secondary">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
               stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
             Wishlist
-          </button>
+          </button> -->
         </div>
 
         <div class="mt-12">
