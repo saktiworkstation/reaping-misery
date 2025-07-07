87d1fc997cebd8da526e1e91d6acd2cc80e731d9
Rplx197 <brafli99197@gmail.com>
2025-07-07 12:01:54 +0700

    test icon

--- components/Features.vue
+++ components/Features.vue
@@ -11,7 +11,9 @@
           class="bg-indigo-900/30 backdrop-blur-sm rounded-xl p-6 border border-indigo-800 hover:border-indigo-600 transition-all duration-300 hover:shadow-lg hover:shadow-indigo-900/50 hover:-translate-y-1">
           <div
             class="flex items-center justify-center w-16 h-16 rounded-full bg-gradient-to-br from-indigo-600 to-pink-600 mb-6 mx-auto">
-            <component :is="feature.icon" class="w-8 h-8 text-white" />
+            
+            <div v-html="feature.icon" class="w-8 h-8 text-white"></div>
+
           </div>
           <h3 class="text-xl font-bold text-white mb-3 text-center">{{ feature.title }}</h3>
           <p class="text-indigo-200 text-center">{{ feature.description }}</p>
@@ -22,46 +24,20 @@
 </template>
 
 <script setup>
-import { defineComponent } from 'vue';
-// Feature icons as components
-const LeafIcon = defineComponent({
-  template: `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-    </svg>
-  `
-})
 
-const PetIcon = defineComponent({
-  template: `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-    </svg>
-  `
-})
-
-const DungeonIcon = defineComponent({
-  template: `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-    </svg>
-  `
-})
-
-// Feature data
 const features = [
   {
-    icon: LeafIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" /></svg>`,
     title: 'Sistem Bertani Simpel',
     description: 'Tanam, panen, dan jual hasil kebun untuk upgrade alat.'
   },
   {
-    icon: PetIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" /></svg>`,
     title: 'Peliharaan Tangguh',
     description: 'Latih pet-mu untuk bertarung atau bantu pekerjaan di kebun.'
   },
   {
-    icon: DungeonIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>`,
     title: 'Dungeon Misterius',
     description: 'Jelajahi dungeon dengan musuh unik dan hadiah langka.'
   }
