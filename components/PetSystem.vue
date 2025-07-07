8a30856450233dd5b21c8a580191f524b7003305
Rplx197 <brafli99197@gmail.com>
2025-07-07 11:36:29 +0700

    update pet chicken

--- components/PetSystem.vue
+++ components/PetSystem.vue
@@ -40,7 +40,7 @@
 const pets = [
   {
     name: 'Chicken',
-    imageUrl: 'https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imageUrl: 'https://img.itch.zone/aW1hZ2UvMzY4ODQ0OC8yMjAzMDIwMS5wbmc=/794x1000/zvoEmj.png',
     tags: ['Healer', 'Slow'],
     description: 'Pet ini berkhusus pada penyembuhan jarak jauh, kekurangannya adalah healnya sangat lama.'
   },
